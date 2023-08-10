61bf4ecae31e293d7328c18b0c6921380310059e
Kevin Nguyen <knth@hey.com>
2023-08-10 17:47:41 +0200

    feat: add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "Oxideo Organizer")]
+#[command(author = "KNTH")]
+#[command(version = "0.1.0")]
+#[command(about = "Automagically sort photos for you!", long_about = None)]
+pub struct Cli {
+    input: String,
+    output: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("Hello, world!!!! {}", cli.input);
 }
