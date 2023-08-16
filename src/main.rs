7d93abb0bf4a80cb5d96b75e4137b254f44a2fca
Kevin Nguyen <knth@hey.com>
2023-08-17 00:53:20 +0200

    feat: resursively parse input directory

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use anyhow::Result;
 use chrono::NaiveDateTime;
 use clap::Parser;
-use std::{
-    fs,
-    io::{self},
-    path::PathBuf,
-};
+use std::{fs, path::PathBuf};
 use tracing::{debug, error, info, instrument, warn, Level};
 
 #[derive(Parser)]
@@ -31,21 +27,43 @@ fn main() -> Result<()> {
         .init();
 
     let cli = Cli::parse();
+
+    let mut non_media_paths: Vec<String> = vec![];
+
+    let mut counter = Counter {
+        all: 0,
+        media: 0,
+        processed: 0,
+    };
+
+    parse_input_dir(&cli.input, &cli.output, &mut counter, &mut non_media_paths)?;
+
+    info!("Successfully parsed the input directory. There are {} files, in which {} are detected media. {} got processed.", 
+        counter.all, counter.media, counter.processed
+    );
+
+    if counter.all > counter.media {
+        for path in non_media_paths {
+            warn!("Non media files: {}", path);
+        }
+    }
+
+    Ok(())
+}
+
+fn parse_input_dir(
+    input: &str,
+    output: &str,
+    counter: &mut Counter,
+    non_media_paths: &mut Vec<String>,
+) -> Result<()> {
     let ext_list = [
         "jpg", "jpeg", "png", "gif", "bmp", "tiff", "ico", "heic", "webp", "svg", "raw", "mp4",
         "mov", "avi", "3gp", "mkv", "flv", "wmv", "mpeg", "webm",
     ];
 
-    let mut non_media_paths: Vec<String> = vec![];
-
-    match fs::read_dir(cli.input) {
+    match fs::read_dir(input) {
         Ok(paths) => {
-            let mut counter = Counter {
-                all: 0,
-                media: 0,
-                processed: 0,
-            };
-
             for path in paths {
                 match path {
                     Ok(dir_entry) => {
@@ -67,8 +85,7 @@ fn main() -> Result<()> {
                                     let format_date = datetime.format("%Y-%m-%d %T").to_string();
                                     debug!("creation time: {format_date}");
 
-                                    let target_path =
-                                        make_dir(cli.output.clone(), format_year, format_month)?;
+                                    let target_path = make_dir(output, format_year, format_month)?;
 
                                     let dest_media_file_name = path.file_name().unwrap();
                                     let mut dest_media_path = PathBuf::from(target_path);
@@ -79,28 +96,24 @@ fn main() -> Result<()> {
                                 }
                                 None => {
                                     warn!("Cannot get media DateTimeOriginal");
-                                    // TODO: copy this into a separate directory
-                                    // fs::copy(&path, &dest_media_path)?;
+                                    make_dir_untouched(&path, output)?;
                                 }
                             }
-                        } else {
+                        } else if path.is_dir() {
+                            parse_input_dir(
+                                path.to_str().unwrap(),
+                                output,
+                                counter,
+                                non_media_paths,
+                            )?;
+                        } else if path.is_file() {
                             non_media_paths.push(path_display.to_string());
+                            make_dir_untouched(&path, output)?;
                         }
                     }
                     Err(e) => tracing::error!("Error reading directory: {}", e),
                 }
                 counter.all += 1;
-            }
-
-            info!(
-                "Successfully parsed the input directory. There are {} files, in which {} are detected media. {} got processed.",
-                counter.all, counter.media, counter.processed
-            );
-
-            if counter.all > counter.media {
-                for path in non_media_paths {
-                    warn!("Non media files: {}", path);
-                }
             }
         }
         Err(e) => {
@@ -110,8 +123,7 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-
-fn make_dir(output_dir: String, year: String, month: String) -> io::Result<String> {
+fn make_dir(output_dir: &str, year: String, month: String) -> Result<String> {
     let mut output_dir = std::path::PathBuf::from(output_dir);
     output_dir.push("");
     let output_dir = output_dir.to_string_lossy().into_owned();
@@ -119,6 +131,26 @@ fn make_dir(output_dir: String, year: String, month: String) -> io::Result<Strin
     fs::create_dir_all(&target_path)?;
 
     Ok(target_path)
+}
+
+fn make_dir_untouched(path: &PathBuf, dest: &str) -> Result<()> {
+    let mut dest_media_path = PathBuf::new();
+
+    dest_media_path.push(dest);
+    dest_media_path.push("untouched");
+    dest_media_path.push(path);
+
+    println!("{:?}", path);
+
+    let mut dest_dir = dest_media_path.clone();
+    dest_dir.pop();
+
+    fs::create_dir_all(dest_dir)?;
+
+    println!("{:?}", dest_media_path);
+    fs::copy(path, &dest_media_path)?;
+
+    Ok(())
 }
 
 fn read_metadata(path: &str) -> Option<NaiveDateTime> {
